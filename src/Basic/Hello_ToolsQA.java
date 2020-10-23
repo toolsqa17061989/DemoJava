e5d6e15c1487a4667530ae9d442d5ca07a9afbb7
toolsqa17061989 <70275231+toolsqa17061989@users.noreply.github.com>
2020-10-23 09:37:35 +0530

    Update Hello_ToolsQA.java

--- src/Basic/Hello_ToolsQA.java
+++ src/Basic/Hello_ToolsQA.java
@@ -4,9 +4,9 @@ public class Hello_ToolsQA {
 
     public static void main(String args[])
     {
-        for(int i=0; i<6; i++)
+        for(int i=0; i<5; i++)
         {
-            System.out.println("Hello ToolsQA Welcome 6");
+            System.out.println("Hello ToolsQA");
         }
     }
 }
