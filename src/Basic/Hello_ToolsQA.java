4ed53b52cebbc6f389c244aa5b4671caf530ac7a
Yutikasree <120539772+Yutikasree@users.noreply.github.com>
2022-12-29 09:45:17 +0530

    print command added

--- src/Basic/Hello_ToolsQA.java
+++ src/Basic/Hello_ToolsQA.java
@@ -4,10 +4,11 @@ public class Hello_ToolsQA {
 
     public static void main(String args[])
     {
-        for(int i=0; i<6; i++)
+        for(int i=0; i<5; i++)
         {
             //Commit for webhooks 2 test
             System.out.println("Hello ToolsQA");
+            System.out.println("Hello Devops Team ");
         }
     }
 }
